10d09b35530f4f38890b2db6d6a18909421d5abd
nuria-alonso <nuria.alonso@colectic.coop>
2018-03-20 10:25:10 +0100

    Corrección de logo, añadir explicación

--- src/js/components/IntroSection.js
+++ src/js/components/IntroSection.js
@@ -8,7 +8,7 @@ export default class IntroSection extends React.Component {
         <div className="row">
         <div className="col col-md-3"></div>
         <div className="col col-md-6">
-          <img alt="Decidim logo" className="logo" src="https://decidim-barcelona.s3.amazonaws.com/uploads/decidim/organization/logo/1/medium_tHG04IS.png" />
+          <img alt="Decidim logo" className="logo" src="https://d33wubrfki0l68.cloudfront.net/c8a989ae15c4def77353ab677084cf68e6a9c754/d6de4/images/decidim-logo.svg" />
         </div>
       </div>
     </div>
